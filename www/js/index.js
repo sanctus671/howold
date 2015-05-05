04b6564c482cb0395fca4cc6dde31e2436e43492
Taylor Hamling <0603671@live.com>
2015-05-06 01:49:09 +1200

    styles

--- www/js/index.js
+++ www/js/index.js
@@ -1,9 +1,30 @@
+var spinner = new Spinner({
+  lines: 17, // The number of lines to draw
+  length: 1, // The length of each line
+  width: 14, // The line thickness
+  radius: 12, // The radius of the inner circle
+  corners: 0.5, // Corner roundness (0..1)
+  rotate: 60, // The rotation offset
+  direction: 1, // 1: clockwise, -1: counterclockwise
+  color: '#FF9716', // #rgb or #rrggbb or array of colors
+  speed: 1.6, // Rounds per second
+  trail: 100, // Afterglow percentage
+  shadow: false, // Whether to render a shadow
+  hwaccel: true, // Whether to use hardware acceleration
+  className: 'spinner', // The CSS class to assign to the spinner
+  zIndex: 2e9, // The z-index (defaults to 2000000000)
+  top: '50%', // Top position relative to parent
+  left: '50%' // Left position relative to parent
+});
 
 var app = {
 
     // Application Constructor
     initialize: function() {
         this.bindEvents();
+		
+		
+		
     },
     // Bind Event Listeners
     //
@@ -79,7 +100,7 @@ var app = {
 	
 	onPhotoSuccess: function(imageURI) {
 		$('.welcome').hide();
-		$('.loader').show();
+		spinner.spin(document.body);
 		var image = document.getElementById('image');
 		image.src = imageURI;
 		$('.box').remove();
@@ -140,23 +161,27 @@ var app = {
 
 				}
 				$('.box').fadeIn();
-				$('.loader').hide();
+				spinner.stop();
 				//clean up
+				/*
 				$.ajax({
 					url: 'http://www.taylorhamling.com/HowOld/delete.php',
 					type: 'POST',
 					data: {url:response.url}
 				});
+				*/
 			})
 			.fail(function() {
-				$('.loader').hide();
+				spinner.stop();
 				alert("No faces detected. Please try again.");
 				//still clean up
+				/*
 				$.ajax({
 					url: 'http://www.taylorhamling.com/HowOld/delete.php',
 					type: 'POST',
 					data: {url:response.url}
 				});
+				*/
 			});
 		});			
 		
@@ -165,7 +190,7 @@ var app = {
 	},
 	
 	fail: function(error){
-		$('.loader').hide();
+		spinner.stop();
 		alert("There was an error. Please try again.");
 	
 	}
